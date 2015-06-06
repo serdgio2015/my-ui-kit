72045bddaffe821cf1eb46808a9731c228e79922
sergey <vengel_sergey@mail.ru>
2015-06-06 21:18:55 +0300

    last-com

---
+++ js/script.js
@@ -0,0 +1,148 @@
+$(document).ready(function() {
+
+
+	$('a').on('click', function(e) {
+		e.preventDefault();
+	})
+
+
+	$('.tabs__item').on('click', function() {
+		$('.tabs__item').removeClass('tabs__item--active');
+		$(this).addClass('tabs__item--active');
+		var paragraf = $(this).attr('href');
+		$('.tabs-paragraf').removeClass('tabs-paragraf--active');
+		$(paragraf).addClass('tabs-paragraf--active');
+	})
+
+
+	var w = $('.interactive__slider-item').width();	
+
+	$('.interactive__slider-right').click(function slid(event) {
+		var l = $('.interactive__slider-list').position().left;
+			if(l > -w*2) {
+			$('.interactive__slider-list').animate({left: l-w});
+		}	
+	})
+
+	$('.interactive__slider-left').click(function slid(event) {
+		var l = $('.interactive__slider-list').position().left;
+			if(l < 0) {
+			$('.interactive__slider-list').animate({left: l+w});
+		}	
+	})
+
+	$('#slid-1').click(function() {
+		$(this).removeClass('interactive__slider-btn').addClass('interactive__slider-btn__active');
+		$('#slid-2').removeClass('interactive__slider-btn__active').addClass('interactive__slider-btn');
+		$('#slid-3').removeClass('interactive__slider-btn__active').addClass('interactive__slider-btn');
+		$('.interactive__slider-list').animate({left: 0});
+	})
+
+	$('#slid-2').click(function() {
+		$(this).removeClass('interactive__slider-btn').addClass('interactive__slider-btn__active');
+		$('#slid-1').removeClass('interactive__slider-btn__active').addClass('interactive__slider-btn');
+		$('#slid-3').removeClass('interactive__slider-btn__active').addClass('interactive__slider-btn');
+		$('.interactive__slider-list').animate({left: -w});
+	})
+
+	$('#slid-3').click(function() {
+		$(this).removeClass('interactive__slider-btn').addClass('interactive__slider-btn__active');
+		$('#slid-1').removeClass('interactive__slider-btn__active').addClass('interactive__slider-btn');
+		$('#slid-2').removeClass('interactive__slider-btn__active').addClass('interactive__slider-btn');
+		$('.interactive__slider-list').animate({left: -w*2});
+	})
+
+
+
+
+
+	$('#circle1').click(function() {
+		$('.steps__step-circle').removeClass('steps__step--active');
+		$('.steps__step-line').removeClass('steps__step--active');
+		$(this).addClass('steps__step--active');
+	})
+
+	$('#circle2').click(function() {
+		$('.steps__step-circle').removeClass('steps__step--active');
+		$('.steps__step-line').removeClass('steps__step--active');
+		$('#circle1').addClass('steps__step--active');
+		$('#line1').addClass('steps__step--active');
+		$(this).addClass('steps__step--active');
+	})
+
+	$('#circle3').click(function() {
+		$('.steps__step-circle').addClass('steps__step--active');
+		$('.steps__step-line').addClass('steps__step--active');
+	})
+
+
+
+
+
+
+
+	 $( "#runner" ).slider({
+      	range: true,
+      	min: 0,
+      	max: 15000,
+      	values: [ 1200, 9500 ],
+      	slide: function( event, ui ) {
+        $( "#amount" ).val(ui.values[ 0 ] + "Р" + "  " + ui.values[ 1 ] + "Р " );
+      }
+    });
+    $( "#amount" ).val($( "#runner" ).slider( "values", 0 ) + "Р" +
+      "  " + $( "#runner" ).slider( "values", 1 ) + "Р" );
+
+
+      $(function() {
+    var progressbar = $( "#progressbar" ),
+      progressLabel = $( ".progress-label" );
+ 
+    progressbar.progressbar({
+      value: false,
+      change: function() {
+        progressLabel.text( progressbar.progressbar( "value" ) + "%" );
+      },
+      complete: function() {
+        progressLabel.text( "100%" );
+      }
+    });
+ 
+    function progress() {
+      var val = progressbar.progressbar( "value" ) || 0;
+ 
+      progressbar.progressbar( "value", val + 2 );
+ 
+      if ( val < 99 ) {
+        setTimeout( progress, 80 );
+      }
+    }
+ 
+    setTimeout( progress, 0 );
+  });
+
+      $('.toggle-unselected2').click(function() {
+      	$('#field-red1').css({'display' : 'none'});
+      	$('#field-red2').css({'display' : 'block'});
+      })
+      $('.toggle-unselected1').click(function() {
+      	$('#field-red2').css({'display' : 'none'});
+      	$('#field-red1').css({'display' : 'block'});
+      })
+
+      $('.messages-list__item__work').hover(function() {
+      	$('.messages-list__list-work').slideToggle();
+      })
+
+
+      $('#swich').click(function() {
+      	if($(this).text() == 'Включено') {
+      		$(this).text('Выключено');
+      	}
+      	else {
+      		$(this).text('Включено');
+      	}
+      })
+
+	
+});
